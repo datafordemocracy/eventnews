f9147789f1e2a9e923cde1d9571a56628447ab7b
gautam678 <gautam678@gmail.com>
2017-11-03 22:30:57 -0400

    fixup! Merge branch 'master' of https://github.com/datafordemocracy/eventnews

--- scraper/getPosts.py
+++ scraper/getPosts.py
@@ -40,8 +40,11 @@ for submission in reddit.subreddit('CornbreadLiberals+GreenParty+Liberal+Sanders
     print 50*'*'
     count_left = count_left+1
 
+'''
+Code for all the posts in right subreddits that correspond to a search query
 
-
+The search being executed is for the query 'charlottesville' 
+'''
 count = 0 
 for submission in reddit.subreddit('Conservative+NewRight+Objectivism+Republican+Romney+Trueobjectivism+conservatives+monarchism+paleoconservative+republicans').search('Charlottesville',limit = 5000,syntax='cloudsearch',sort = 'top'):
     url = submission.url
@@ -54,14 +57,25 @@ for submission in reddit.subreddit('Conservative+NewRight+Objectivism+Republican
     print "Domain", source.netloc  # Output: the submisson's domain
     print 50*'*'
     count = count+1
+
+# Print the number of articles extracted from each collective subreddits
 print
 print "Number of articles from left: ",count_left
 print "Number of articles from right",count
 print
+
+# Calculate the number of times each source is referenced in their respective subreddits
+
 left = Counter(left_score)  # Frequency calculation
 right = Counter(right_score)
+
+# Sort the list putting most referenced sources in the top
+
 left = left.most_common()  # Most_common sorts the list based on how often they have occured
 right = right.most_common()
+
+# Print the values in console
+
 print '{} {: >30s}'.format("Left","Right")
 for i,j in zip(left[0:10],right[0:10]):   # Here I'm taking the top 10 articles
     print '{}: {} , {}: {}'.format(i[0],i[1],j[0],j[1])
